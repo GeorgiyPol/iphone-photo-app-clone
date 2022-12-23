04602c18a6b252b053dd7483482a7bde7d941d4e
Georgiy <33875528+georg1856@users.noreply.github.com>
2022-12-23 10:22:20 +1000

    create model

---
+++ iphone photo app clone/iphone photo app clone/Sources/Scenes/Model/CellModel.swift
@@ -0,0 +1,14 @@
+//
+//  CellModel.swift
+//  iphone photo app clone
+//
+//  Created by Georgiy on 23.12.2022.
+//
+
+import Foundation
+
+struct CellModel: Hashable {
+    let imageName: String
+    let labelName: String
+    let labelCount: String
+}
